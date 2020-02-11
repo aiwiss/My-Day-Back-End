133e24b61d7006ad93921660a287b29a0b9e2c5a
Aivaras Jonikas <jonikasa@coventry.ac.uk>
2020-02-11 21:31:43 +0000

    test4

--- src/posts/routes.js
+++ src/posts/routes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const roles = require('../users/roles');
 const auth = require('../middleware/auth');
-const db = require('../db/memorydb-handler');
 
 const service = require('../posts/postService');
 
